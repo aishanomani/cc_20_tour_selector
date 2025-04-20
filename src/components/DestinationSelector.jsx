ee6c1c7e85e509ba04cc0a17ec4c91d9f176d33d
Aisha Nomani <aishan@usf.edu>
2025-04-20 16:35:41 -0400

    Updated DestinationSelector.jsx

--- src/components/DestinationSelector.jsx
+++ src/components/DestinationSelector.jsx
@@ -1,4 +1,24 @@
-function DestinationSelector() {
-    return <div>DestinationSelector component</div>;
-  }
-  export default DestinationSelector;
+import React from "react";
+
+const DestinationSelector = ({ tours, selected, setSelected }) => {
+  const uniqueNames = ["All", ...new Set(tours.map((tour) => tour.name))];
+
+  return (
+    <div className="destination-selector" style={{ marginBottom: "20px" }}>
+      <label htmlFor="tour-select">Filter by Destination: </label>
+      <select
+        id="tour-select"
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)}
+      >
+        {uniqueNames.map((name, index) => (
+          <option key={index} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default DestinationSelector;
