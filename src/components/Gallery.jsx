5af4450912c8e7fee90bd6302554fb370d63cd76
Aisha Nomani <aishan@usf.edu>
2025-04-20 16:24:51 -0400

    Updated Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,26 @@
-function Gallery() {
-    return <div>Gallery component</div>;
+import React from "react";
+import TourCard from "./TourCard";
+
+const Gallery = ({ tours, onRemove }) => {
+  if (!tours.length) {
+    return <p>No tours to display.</p>;
   }
-  export default Gallery;
+
+  return (
+    <section className="gallery" style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+      {tours.map((tour) => (
+        <TourCard
+          key={tour.id}
+          id={tour.id}
+          name={tour.name}
+          info={tour.info}
+          price={tour.price}
+          image={tour.image}
+          onRemove={onRemove}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Gallery;
